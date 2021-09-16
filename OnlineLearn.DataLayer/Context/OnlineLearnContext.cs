98680b09e3a3cb71ee8a1c0cfe2f5c47a95a6b31
Morteza Keivani <mortezakeivani.69@gmail.com>
2021-09-16 16:20:57 +0430

    Add project files.

---
+++ OnlineLearn.DataLayer/Context/OnlineLearnContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineLearn.DataLayer.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLearn.DataLayer.Context
+{
+    public class OnlineLearnContext:DbContext
+    {
+        public OnlineLearnContext(DbContextOptions<OnlineLearnContext> options):base(options)
+        {
+
+        }
+
+        #region User
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        #endregion
+    }
+}
