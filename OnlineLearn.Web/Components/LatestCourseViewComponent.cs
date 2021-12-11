2e71c4885161f9535e41378eca19ad14b6eda5eb
Morteza Keivani <mortezakeivani.69@gmail.com>
2021-12-11 12:36:54 +0330

    Cache _CourseBox ViewComponent

---
+++ OnlineLearn.Web/Components/LatestCourseViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineLearn.Core.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace OnlineLearn.Web.Components
+{
+    public class LatestCourseViewComponent:ViewComponent
+    {
+        private readonly ICourseService _courseService;
+
+        public LatestCourseViewComponent(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult((IViewComponentResult)View("LatestCourse", _courseService.GetCourses().Item1));
+        }
+    }
+}
